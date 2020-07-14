ad618a820d59933e527a755756d88e1b7936460f
jesusChristian18 <chris.jesus1997@gmail.com>
2020-07-14 12:18:30 -0500

    validacion

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -3,12 +3,23 @@ import { RegistroService } from "../../services//registro.service";
 import { User } from "../../model/user";
 import { Router, ActivatedRoute } from "@angular/router";
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import Swal from "sweetalert2";
+
+
 @Component({
   selector: "app-registro",
   templateUrl: "./registro.component.html",
   styleUrls: ["./registro.component.css"],
+  
 })
 export class RegistroComponent implements OnInit {
+createFormGroup(){
+  return new FormGroup({
+    Nombres: new FormControl('')
+  })
+}
+
   //Model user to send - Register
   userM: User = {
     idUser: 0,
@@ -20,7 +31,9 @@ export class RegistroComponent implements OnInit {
     Nombres: "",
     idRol: 0,
     NombreEmpresa: "",
-    telefono: 123434567,
+    telefono: "",
+    Rpass: "",
+   
   };
 
   userE: User = {
@@ -33,19 +46,28 @@ export class RegistroComponent implements OnInit {
     Nombres: "",
     idRol: 0,
     NombreEmpresa: "",
-    telefono: 1234567,
+    telefono: "",
+    Rpass: "",
   };
+
 
   //Listar Usuarios
   user: any = [];
+
+  contactForm: FormGroup
   constructor(
     private registroService: RegistroService,
     private router: Router,
     private activtedRoute: ActivatedRoute
-  ) {}
-
+  ) {
+    this.contactForm = this.createFormGroup();
+  }
+  validatingForm: FormGroup;
   ngOnInit() {
     this.getUsers();
+    this.validatingForm = new FormGroup({
+      text: new FormControl(null, [Validators.required, Validators.email])
+    });
   }
 
   getUsers() {
@@ -58,7 +80,70 @@ export class RegistroComponent implements OnInit {
     );
   }
 
+  esEmailValido(email: string):boolean {
+    let mailValido = false;
+      'use strict';
+
+      var EMAIL_REGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/;
+
+      if (email.match(EMAIL_REGEX)){
+        mailValido = true;
+      }
+    return mailValido;
+  }
+
+  
+
+
+
   saveNewUserPersona() {
+    //var pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2-3}$"
+    let esValido:boolean = true;
+    console.log(this.userM)
+    console.log(this.userM.telefono.length);
+    console.log(this.userM.DNI.length);
+   if (this.userM.Apellidos == "" || this.userM.DNI == "" || this.userM.email== "" || this.userM.pass == "" || this.userM.telefono == "" || this.userM.Nombres == ""    ) {
+    Swal.fire({
+      icon: "warning",
+      title: "Datos Faltantes, completa los campos vacios",
+      showConfirmButton: false,
+      timer: 2000,
+    }).then((result) => {
+    });
+   
+   }else if(this.userM.telefono.length<8){
+    Swal.fire({
+      icon: "warning",
+      title: "Celular incorrecto, vuelve intentarlo nuevamente",
+      showConfirmButton: false,
+      timer: 2000,
+    }).then((result) => {
+    });
+   }else if(!this.esEmailValido(this.userM.email)){
+    Swal.fire({
+      icon: "warning",
+      title: "Correo incorrecto, vuelve intentarlo nuevamente",
+      showConfirmButton: false,
+      timer: 2000,
+    }).then((result) => {
+    });
+   }else if(this.userM.DNI.length<8){
+    Swal.fire({
+      icon: "warning",
+      title: "DNI incorrecto, vuelve a ponerlo nuevamente",
+      showConfirmButton: false,
+      timer: 2000,
+    }).then((result) => {
+    });
+   }else if(this.userM.pass!=this.userM.Rpass){
+    Swal.fire({
+      icon: "warning",
+      title: "Las contraseñas no coiciden, vuelve a intarlo por favor!!",
+      showConfirmButton: false,
+      timer: 2000,
+    }).then((result) => {
+    });
+   }else{
     delete this.userM.idUser;
     this.userM.idRol = 2;
     this.registroService.saveUser(this.userM).subscribe(
@@ -73,22 +158,106 @@ export class RegistroComponent implements OnInit {
       }
     );
     return this.registroService;
+   
   }
 
+ 
+}
+
+
+
+
+
+
   saveNewUserEmpresa() {
-    delete this.userE.idUser;
-    this.userE.idRol = 1;
-    this.registroService.saveUser(this.userE).subscribe(
-      (res) => {
-        console.log(res);
-        /*         this.registroService.setToken(res["token"]);
-         */ this.router.navigate(["/registro"]);
-        this.router.navigateByUrl("/");
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
-    return this.registroService;
+    console.log(this.userE)
+     
+    
+    console.log( this.userE.telefono.length);
+    if (this.userE.NombreEmpresa == "" || this.userE.RUC == "" || this.userE.telefono== "" || this.userE.email == "" || this.userE.pass == "") {
+      Swal.fire({
+        icon: "warning",
+        title: "Datos Faltantes, completa los campos vacios",
+        showConfirmButton: false,
+        timer: 2000,
+      }).then((result) => {
+      });
+     
+     }else if(this.userE.RUC.length<12){
+      Swal.fire({
+        icon: "warning",
+        title: "RUC incorrecto,  vuelve a intarlo por favor!!",
+        showConfirmButton: false,
+        timer: 2000,
+      }).then((result) => {
+      });
+     }else if(this.userE.telefono.length<9){
+      Swal.fire({
+        icon: "warning",
+        title: "Celular incorrecto,  vuelve a intarlo por favor!!",
+        showConfirmButton: false,
+        timer: 2000,
+      }).then((result) => {
+      });
+     }else if(!this.esEmailValido(this.userE.email)){
+      Swal.fire({
+        icon: "warning",
+        title: "Correo incorrecto, vuelve intentarlo nuevamente",
+        showConfirmButton: false,
+        timer: 2000,
+      }).then((result) => {
+      });
+     }
+     else if(this.userE.pass!=this.userE.Rpass){
+      Swal.fire({
+        icon: "warning",
+        title: "Las contraseñas no coiciden, vuelve a intarlo por favor!!",
+        showConfirmButton: false,
+        timer: 2000,
+      }).then((result) => {
+      });
+     }else{
+      delete this.userE.idUser;
+      this.userE.idRol = 1;
+      this.registroService.saveUser(this.userE).subscribe(
+        (res) => {
+          console.log(res);
+          /*         this.registroService.setToken(res["token"]);
+           */ this.router.navigate(["/registro"]);
+          this.router.navigateByUrl("/");
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+      return this.registroService;
+     }
+
+
+    
   }
+
+  ValidaSoloNumeros(skill:any) {
+   
+   
+    console.log(skill.charCode)
+    if ((skill.charCode < 48) || (skill.charCode > 57) ) 
+      skill.returnValue = false;
+
+      
+          //No esta permitido - eliminar 
+  }
+
+  soloLetras(skill:any) {
+   
+   
+    console.log(skill.charCode)
+    if ((skill.charCode <97 ) || (skill.charCode > 122) ) 
+      skill.returnValue = false;
+
+      
+          //No esta permitido - eliminar 
+  }
+  
+
 }
